22991f3d3d8ab365020bb27dc735dba4a723b9b9
Afissard <sacha.chauvel@gmail.com>
2023-05-07 12:43:21 +0200

    add project information in main.c

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
+/* 
+PROJECT INFORMATIONS :
+This is a side project of my current projet of creating a full
+tui app without any other library than the standard one's in C.
+
+The main goal of this side project is to create a sort of
+esoteric programming language, turing complete, of my own...
+
+PS (1) : this is a even bader idea than the my previous goal of 
+creating a brainfuck interpreter.
+PS (2) : "Cerebri Igni" mean "Brain in fire" in Latin. This is
+just the code name of my project. 
+
+TODO :
+- All
+
+OTHER INFORMATIONS :
+compile : gcc main.c -o cerebri
+run     : ./cerebri
+
+*/
+
 #include <stdio.h>
 
 int main(){
-    printf("Cerebri Igni");
+    printf("Cerebri Igni!\n");
     return 0;
 }
