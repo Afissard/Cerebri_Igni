0d76b90fd03d83458ef069536fc0b61e12d9c036
Afissard <sacha.chauvel@gmail.com>
2023-05-13 15:56:05 +0200

    Conway Game of Life in C finish, will add template

--- Conway_Game_of_Life.c
+++ Conway_Game_of_Life.c
@@ -1,35 +1,81 @@
+/*
+Information :
+Compile : gcc Conway_Game_of_Life.c -o life -l ncurses
+*/
+
 #include "Conway_Game_of_Life.h"
 
+
 int main(){
+    // ncurses setup
+    initscr();
+    //curs_set(0);
+    cbreak();
+    noecho(); // do not print the user input
+    //nodelay(stdscr, true); // do not wait for the user input
+    keypad(stdscr, TRUE);
+
+    if(has_colors() == FALSE){	
+        endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+    start_color();
+    init_pair(BLACK, COLOR_BLACK, COLOR_BLACK);
+    init_pair(WHITE, COLOR_WHITE, COLOR_WHITE);
+
     init();
     simulation();
+
+    // ncurses end
+    mvprintw(40, 0, "Press anything to quit");
+	getch();
+	endwin();
     return 0;
+}
+
+void delay(int number_of_seconds){
+    int milli_seconds = 1000 * number_of_seconds; // Converting time into milli_seconds
+    clock_t start_time = clock(); // Storing start time
+    while (clock() < start_time + milli_seconds){;} // looping till required time is not achieved
 }
 
 void init(){
     /* Initialisation of the game of life */
     seed = rand();
     srand(seed); // set seed when compiled
-    printf("Seed : %d\n", seed);
+    mvprintw(33, 0, "Seed : %d", seed);
 
-    printf("Enter the number of generations : ");
-    scanf("%d", &gen_nbr);
+    /*
+    mvprintw(34, 0, "Enter the number of generations : ");
+    gen_nbr = getch();
+    mvprintw(34, 0, "Number of generations targeted : %d.", gen_nbr);
+    */
 
-    for(int y=0; y < TAB_SIZE-1; y++){
-        for(int x=0; x < TAB_SIZE-1; x ++){
+    for(int y=0; y < TAB_Y_SIZE-1; y++){
+        for(int x=0; x < TAB_X_SIZE-1; x ++){
                 tab[y][x] =  rand() % 2; // generate random number between 0 and 1;
                 tab_buff[y][x] = tab[y][x];
-                printf("%d", tab[y][x]);
+                if(tab[y][x] == 0){
+                    attron(COLOR_PAIR(BLACK));
+                    mvprintw(y, x, "%d", tab[y][x]);
+                    attroff(COLOR_PAIR(BLACK));
+                }
+                else{
+                    attron(COLOR_PAIR(WHITE));
+                    mvprintw(y, x, "%d", tab[y][x]);
+                    attroff(COLOR_PAIR(WHITE));
+                }
+                
         }
-        printf("\n");
     }
-    printf("Setup done.\n");
+    //printf("Setup done.\n");
 }
 
 void update(){
     /*Apply the rule of the game of life*/
-    for(int y=0; y < TAB_SIZE-1; y++){
-        for(int x=0; x < TAB_SIZE-1; x ++){
+    for(int y=0; y < TAB_Y_SIZE-1; y++){
+        for(int x=0; x < TAB_X_SIZE-1; x ++){
 
             int neighbors_nbr;
             // TODO: prevent going out of range of the board: may cause memory leaks ?
@@ -52,18 +98,50 @@ void update(){
 }
 
 void simulation(){
-    // Simulation part
+    /*Run the Game of Life*/
+    nodelay(stdscr, true); // do not wait for the user input
+    int ch =0;
+    while (1){
+        ch = getch();
+        if(ch == KEY_UP){nodelay(stdscr, false); break;}
+        mvprintw(40, 0, "To end press \"KEY_UP\".");
+
+        mvprintw(35, 0, "Generation number %d.", gen);
+        gen++;
+        update();
+
+        for(int y=0; y < TAB_Y_SIZE-1; y++){
+            for(int x=0; x < TAB_X_SIZE-1; x ++){
+                tab[y][x] = tab_buff[y][x];
+                if(tab[y][x] == 0){
+                    attron(COLOR_PAIR(BLACK));
+                    mvprintw(y, x, "%d", tab[y][x]);
+                    attroff(COLOR_PAIR(BLACK));
+                }
+                else{
+                    attron(COLOR_PAIR(WHITE));
+                    mvprintw(y, x, "%d", tab[y][x]);
+                    attroff(COLOR_PAIR(WHITE));
+                }
+            }
+        }
+        refresh();
+        delay(500);
+    }
+    /*
     for(int gen=0; gen < gen_nbr; gen++){
-        printf("Generation number %d.\n", gen);
+        move(35, 0);
+        printw("Generation number %d.", gen);
 
         update();
 
         for(int y=0; y < TAB_SIZE-1; y++){
             for(int x=0; x < TAB_SIZE-1; x ++){
                 tab[y][x] = tab_buff[y][x];
-                printf("%d", tab[y][x]);
+                mvprintw(y, x, "%d", tab[y][x]);
             }
-            printf("\n");
         }
-    }
+        refresh();
+        delay(100);
+    }*/
 }
--- Conway_Game_of_Life.h
+++ Conway_Game_of_Life.h
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ncurses.h>
 
-#define TAB_SIZE 16
+#define BLACK 1
+#define WHITE 2
+#define TAB_Y_SIZE 32
+#define TAB_X_SIZE 128
 
-int tab[TAB_SIZE][TAB_SIZE] = {0};
-int tab_buff[TAB_SIZE][TAB_SIZE] = {0};
+int tab[TAB_Y_SIZE][TAB_X_SIZE] = {0};
+int tab_buff[TAB_Y_SIZE][TAB_X_SIZE] = {0};
 
-int seed =0;
-int gen_nbr =0;
+int seed = 0;
+unsigned long gen = 0;
+int gen_nbr = 100; // default number (and prevent error)
 
+void delay(int);
 void init();
 void update();
 void simulation();
